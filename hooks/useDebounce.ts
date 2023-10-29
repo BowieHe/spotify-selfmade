6dbf22d7fb43b7bd190b0681c38c4806168d3423
Bowie He <hbw234@outlook.com>
2023-10-29 10:08:21 +0800

    feat:add search function

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    console.log(`value in debounce ${value}`);
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
